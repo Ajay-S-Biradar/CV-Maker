f4807f5f98a17fab2c1402d0fa0d6bd193b7ac83
Ajay-S-Biradar <sbmunnu@gmail.com>
2023-07-05 17:35:48 +0530

    changed the array error

--- static/data.js
+++ static/data.js
@@ -1,7 +1,23 @@
 function processData(data) {
     // Use the fetched data here
-
-    console.log(data.tech)
+    const skill = document.querySelector('#sk')
+    console.log(skill) 
+    if(typeof(data.skills)=='string'){
+        let l = document.createElement('li')
+        // li.classList.add('skills') 
+        l.innerText = data.skills
+        skill.append(l)
+    }
+    else{ 
+        size = (data.skills.length)
+        for(i=0;i<size;i++){
+            let li = document.createElement('li')
+            // li.classList.add('skills') 
+            li.innerText = data.skills[i]
+            skill.append(li)
+            }
+    }
+    console.log(data.skills)
     const name = document.querySelector("#JOE-SMITH")
     const Location = document.querySelector('.Location')
     const cell = document.querySelector('.Cellphone')
@@ -10,16 +26,22 @@ function processData(data) {
     const job = document.querySelector('#job')
     const education = document.querySelector('.degree')
     const technology = document.querySelector('#tech')
-    console.log(technology)
-    n = data.tech.length
-    for(i=0;i<n;i++){
+    
+    if(typeof(data.tech)=='string'){
         let l = document.createElement('li')
-        // li.classList.add('skills') 
-        l.innerText = data.tech[i] 
+        // li.classList.add('skills')  
+        l.innerText = data.tech
         technology.append(l)
-    }  
-    // const college = document.querySelector('#cname')
-    // const 
+    }
+    else{
+        n = data.tech.length
+        for(i=0;i<n;i++){
+            let l = document.createElement('li')
+            // li.classList.add('skills') 
+            l.innerText = data.tech[i] 
+            technology.append(l)
+        }
+    }
 
     for(i=0;i<data.eduli.length;i++){
         const div = document.createElement('div')
@@ -30,15 +52,13 @@ function processData(data) {
         let college = document.createElement('p')
         college.innerText = data.eduli[i++]
         time.innerText = data.eduli[i] 
-        let li = document.createElement('li')
+        let li = document.createElement('li') 
         div.append(para) 
         div.append(time)
         li.append(div)
         li.append(college)
         education.append(li) 
     }
-
-
     job.innerText = data.job
     var github = data.github
     git.innerHTML = github
@@ -46,16 +66,7 @@ function processData(data) {
     cell.innerText = data.number
     Location.innerText = data.location
     name.innerText = data.name
-    const skills = document.querySelector('#sk')
-    size = (data.skills.length)
-    for(i=0;i<size;i++){
-    let li = document.createElement('li')
-    // li.classList.add('skills') 
-    li.innerText = data.skills[i]
-    skills.append(li)
-    }
     
-
 
   }
   
@@ -64,12 +75,3 @@ fetch('/api/data')
     .then((data)=> {
         processData(data) 
     })
-
-
-// const access = document.querySelector('#degree')
-// access.firstChild.innerText = 'higher school'
-// access.lastChild.innerText = 'jun 21 - sept 25'
-
-// const education = document.querySelector('.education')
-// console.log(education)
-
