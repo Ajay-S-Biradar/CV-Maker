12ab34d08ce6686ee65e2f882400188aa3166988
Ajay-S-Biradar <123857308+Ajay-S-Biradar@users.noreply.github.com>
2023-07-30 10:42:49 +0530

    Update data.js

--- static/data.js
+++ static/data.js
@@ -1,77 +1,71 @@
-function processData(data) {
+function process(data) {
     // Use the fetched data here
-    const skill = document.querySelector('#sk')
-    console.log(skill) 
-    if(typeof(data.skills)=='string'){
-        let l = document.createElement('li')
-        // li.classList.add('skills') 
-        l.innerText = data.skills
-        skill.append(l)
-    }
-    else{ 
-        size = (data.skills.length)
-        for(i=0;i<size;i++){
-            let li = document.createElement('li')
-            // li.classList.add('skills') 
-            li.innerText = data.skills[i]
-            skill.append(li)
-            }
-    }
-    console.log(data.skills)
+    console.log(data)
     const name = document.querySelector("#JOE-SMITH")
     const Location = document.querySelector('.Location')
     const cell = document.querySelector('.Cellphone')
     const mail = document.querySelector('.Email')
     const git = document.querySelector('.github')
-    const job = document.querySelector('#job')
-    const education = document.querySelector('.degree')
-    const technology = document.querySelector('#tech')
-    
-    if(typeof(data.tech)=='string'){
-        let l = document.createElement('li')
-        // li.classList.add('skills')  
-        l.innerText = data.tech
-        technology.append(l)
-    }
-    else{
-        n = data.tech.length
-        for(i=0;i<n;i++){
-            let l = document.createElement('li')
-            // li.classList.add('skills') 
-            l.innerText = data.tech[i] 
-            technology.append(l)
-        }
-    }
-
-    for(i=0;i<data.eduli.length;i++){
-        const div = document.createElement('div')
-        const para = document.createElement('p')
-        para.innerText = data.eduli[i++]
-        para.classList.add('sanserif')
-        let time = document.createElement('time')
-        let college = document.createElement('p')
-        college.innerText = data.eduli[i++]
-        time.innerText = data.eduli[i] 
-        let li = document.createElement('li') 
-        div.append(para) 
-        div.append(time)
-        li.append(div)
-        li.append(college)
-        education.append(li) 
-    }
-    job.innerText = data.job
-    var github = data.github
-    git.innerHTML = github
+    // const 
+    console.log(git.innerHTML)
+    // var github = data.github
+    // git.innerHTML = github
     mail.innerText = data.email
     cell.innerText = data.number
-    Location.innerText = data.location
-    name.innerText = data.name
-    
-
+    Location.innerText =data.address + "," + data.city 
+    name.innerText = data.name1[0]+" " +data.name1[1]
+    // const skills = document.querySelector('#sk')
+    // size = (data.skills.length)
+    // for(i=0;i<size;i++){
+    // const li = document.createElement('li')
+    // li.classList.add('skills')
+    // li.innerText = data.skills[i]
+    // skills.append(li)
+    // }
   }
-  
-fetch('/api/data')
+
+function process2(data){
+    console.log(data)
+
+    const edu = document.querySelector('.edu')
+    ul = edu.childNodes[0]
+    const li1 = edu.childNodes[0]
+    const li2 = edu.childNodes[1]
+    const li3 = edu.    childNodes[2]
+    p1 = document.createElement('p')
+    p1.innerText = data.school
+    p2 = document.createElement('p')
+    p2.innerText = data.college
+    p3 = document.createElement('p')
+    p3.innerText = data.degree
+    li1.append(p1)
+    li2.append(p2)
+    li3.append(p3)
+
+    const exp = document.querySelector('.experience')
+    const ol = exp.childNodes[0]
+    let li = document.createElement('li')
+    li.innerText = data.exp
+    ol.append(li)
+    
+    const skills = document.querySelector('#sk')
+    size = (data.skills.length)
+    for(i=0;i<size;i++){
+    const li = document.createElement('li')
+    li.classList.add('skills')
+    li.innerText = data.skills[i]
+    skills.append(li)
+    }
+}
+
+fetch('api/data1')
     .then(res=>res.json())
-    .then((data)=> {
-        processData(data) 
+    .then((data)=>{
+    process(data)
+})
+
+fetch('api/data2')
+    .then(res=>res.json())
+    .then((data2)=>{
+        process2(data2)
     })
